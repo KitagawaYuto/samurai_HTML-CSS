1c349aac63f44f2938ec361d6ee5c3d8d0afb85e
unknown <yuto.kita121@gmail.com>
2021-07-27 11:22:55 +0900

    Rename a file 'index.html'

--- JavaScript/sample.js
+++ JavaScript/sample.js
@@ -1,11 +1,8 @@
 
-<body>
-    <script>
-    let myString = 'ユーザー名';
+let kakaku = 100
+let urine = kakaku * 1.08;
+console.log( urine );
+let text = prompt('入力せよ');
+console.log(text);
 
-myString = '太郎';
 
-console.log('ようこそ、' + myString + 'さん');
-
-    </script>
-</body>
